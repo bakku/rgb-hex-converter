363c38b3ef347ebc9d9edd3b5db2b05d9a50d658
Christian Paling <christian.paling@googlemail.com>
2015-03-05 20:59:41 +0100

    hex to rgb functional

--- javascript/converter.js
+++ javascript/converter.js
@@ -1,22 +1,43 @@
 $( document ).ready(function() {
 	
 	$('#hex-to-rgb-convert').click(function() {
-		var reg = /(^[0-9A-F]{3}$)|(^[0-9A-F]{6}$)/i;
+		var regEx = /(^[0-9A-F]{3}$)|(^[0-9A-F]{6}$)/i; 
 		var color = $('#hex-to-rgb-input').val();
+		var result;
 		
-		if (reg.test(color) == false) {
+		if (regEx.test(color) == false) {
 			$('#hex-to-rgb-group').addClass('has-error');
 			$('#hex-to-rgb-icon').removeClass('icon-visible');
-			return;
 		}
+		
+		result = hexToRgb(color);
 		
 		var hashtag = "#";
 		var bgcolor = hashtag.concat(color);
 		
-		$('#hex-to-rgb-result').val('hallo');
+		$('#hex-to-rgb-result').val(result);
 		$('#hex-to-rgb-color').css('background-color', bgcolor);
 		$('#hex-to-rgb-group').removeClass('has-error');
 		$('#hex-to-rgb-icon').addClass('icon-visible');
 	});
 	
 });
+
+function hexToRgb(color) {
+		var rVal;
+		var gVal;
+		var bVal;
+		
+		if (color.length == 3) {
+			rVal = parseInt("0x" + color.charAt(0) + color.charAt(0));
+			gVal = parseInt("0x" + color.charAt(1) + color.charAt(1));
+			bVal = parseInt("0x" + color.charAt(2) + color.charAt(2));
+		}
+		else {
+			rVal = parseInt("0x" + color.substr(0, 2));
+			gVal = parseInt("0x" + color.substr(2, 2));
+			bVal = parseInt("0x" + color.substr(4, 2));
+		}
+		
+		return rVal.toString() + ", " + gVal.toString() + ", " + bVal.toString();
+}
